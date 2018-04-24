26a3a1acbde35db5ecf25377a265d4e3a85429c1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-24 12:38:58 +0000

    Deploy NewYorkCoin-NYC/NYC-site to github.com/NewYorkCoin-NYC/NYC-site.git:gh-pages

---
+++ README.md
@@ -0,0 +1,7 @@
+# NYC-website
+## Deploying to Github Pages:
+- run `npm install ` (if first time or dependencies have changed)
+- to deploy use `npm run deploy` on the branch you wish to deploy
+- to view [QA Site (click here)](https://newyorkcoin-nyc.github.io/NYC-site/)
+
+## Email addresses obfuscated with: [HTML Entities)](http://www.wbwip.com/wbw/emailencoder.html)
---
+++ js/main.js
@@ -0,0 +1,310 @@
+
+
+
+
+
+
+
+
+$(window).load(function(){
+  $('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+
+
+
+$(function($){
+        $(window).load(function(){
+            $("nav a").mPageScroll2id();
+        });
+    });
+
+
+
+$(function($){
+        $(window).load(function(){
+            $(".buttons-header a").mPageScroll2id();
+        });
+    });
+
+/*---------------
+	Fixed menu
+------------------*/
+
+// $(window).scroll(function() {    
+//     var scroll = $(window).scrollTop();
+
+//     if (scroll >= 71) {
+//         // document.getElementById('logo').src="img/logo.png";
+//         $(".navbar-default").addClass("navbar-default-black-bg")
+//         $(".navbar-default").removeClass("navbar-padding")
+//         $(".menu-color").addClass("menu-color-black")
+         
+
+
+//     } else {
+//         // document.getElementById('logo').src="img/logo-white.png";
+//         $(".navbar-default").removeClass("navbar-default-black-bg")
+//         $(".navbar-default").addClass("navbar-padding")
+//         $(".menu-color").removeClass("menu-color-black")
+        
+//     }
+// });
+
+
+
+
+var clientHeight = $( window ).height();
+  $('.header').css('height', clientHeight);
+
+var clientHeight = $( window ).height();
+  $('.header-overlay').css('height', clientHeight);
+
+
+
+
+$('.owl-carousel').owlCarousel({
+    items : 3,
+    loop:false,
+    autoplay:false,
+    responsiveRefreshRate : 100,
+    dots:false,
+    stopOnHover : true,
+    mouseDrag : true,
+    touchDrag : true,
+    nav:true,
+
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        500:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    },
+
+    navText:[
+            "<i class='fa fa-angle-left fa-2x prod-ctrl-l'></i>",
+            "<i class='fa fa-angle-right fa-2x prod-ctrl-r'></i>"
+        ]
+
+})
+
+
+$('.owl-carousel1').owlCarousel({
+    items : 5,
+    loop:false,
+    autoplay:false,
+    responsiveRefreshRate : 100,
+    dots:false,
+    stopOnHover : true,
+    mouseDrag : true,
+    touchDrag : true,
+    nav:true,
+
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        500:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    },
+
+    navText:[
+            "<i class='fa fa-angle-left fa-2x prod-ctrl-l'></i>",
+            "<i class='fa fa-angle-right fa-2x prod-ctrl-r'></i>"
+        ]
+
+})
+
+
+
+
+$('.owl-carousel2').owlCarousel({
+    items : 5,
+    loop:false,
+    autoplay:false,
+    responsiveRefreshRate : 100,
+    dots:false,
+    stopOnHover : true,
+    mouseDrag : true,
+    touchDrag : true,
+    nav:true,
+
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        500:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    },
+
+    navText:[
+            "<i class='fa fa-angle-left fa-2x prod-ctrl-l'></i>",
+            "<i class='fa fa-angle-right fa-2x prod-ctrl-r'></i>"
+        ]
+
+})
+
+
+
+
+
+
+
+
+$(window).load(function(){
+  $('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+
+
+
+$(function($){
+        $(window).load(function(){
+            $("nav a").mPageScroll2id();
+        });
+    });
+
+
+
+$(function($){
+        $(window).load(function(){
+            $(".buttons-header a").mPageScroll2id();
+        });
+    });
+
+/*---------------
+	Fixed menu
+------------------*/
+
+// $(window).scroll(function() {    
+//     var scroll = $(window).scrollTop();
+
+//     if (scroll >= 71) {
+//         // document.getElementById('logo').src="img/logo.png";
+//         $(".navbar-default").addClass("navbar-default-black-bg")
+//         $(".navbar-default").removeClass("navbar-padding")
+//         $(".menu-color").addClass("menu-color-black")
+         
+
+
+//     } else {
+//         // document.getElementById('logo').src="img/logo-white.png";
+//         $(".navbar-default").removeClass("navbar-default-black-bg")
+//         $(".navbar-default").addClass("navbar-padding")
+//         $(".menu-color").removeClass("menu-color-black")
+        
+//     }
+// });
+
+
+
+
+var clientHeight = $( window ).height();
+  $('.header').css('height', clientHeight);
+
+var clientHeight = $( window ).height();
+  $('.header-overlay').css('height', clientHeight);
+
+
+
+
+$('.owl-carousel').owlCarousel({
+    items : 3,
+    loop:false,
+    autoplay:false,
+    responsiveRefreshRate : 100,
+    dots:false,
+    stopOnHover : true,
+    mouseDrag : true,
+    touchDrag : true,
+    nav:true,
+
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        500:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    },
+
+    navText:[
+            "<i class='fa fa-angle-left fa-2x prod-ctrl-l'></i>",
+            "<i class='fa fa-angle-right fa-2x prod-ctrl-r'></i>"
+        ]
+
+})
+
+
+$('.owl-carousel1').owlCarousel({
+    items : 5,
+    loop:false,
+    autoplay:false,
+    responsiveRefreshRate : 100,
+    dots:false,
+    stopOnHover : true,
+    mouseDrag : true,
+    touchDrag : true,
+    nav:true,
+
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        500:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    },
+
+    navText:[
+            "<i class='fa fa-angle-left fa-2x prod-ctrl-l'></i>",
+            "<i class='fa fa-angle-right fa-2x prod-ctrl-r'></i>"
+        ]
+
+})
+
+
+
+
+
+
+ 
